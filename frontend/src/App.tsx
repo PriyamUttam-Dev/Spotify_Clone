c25c11e7c53f5b2f624e98da1197e609ca7b12ea
Priyam Uttam <priyamuttam.dev@gmail.com>
2025-01-12 00:31:59 +0530

    Backend Setup Done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react"
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
+import { Button } from "./components/ui/button"
 
 export default function Home() {
   return (
     <div>
        <header>
       <SignedOut>
-        <SignInButton />
+        <SignInButton>
+          <Button>Sign In</Button>
+        </SignInButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
