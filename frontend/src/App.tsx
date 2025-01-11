d080e202553b28da29ec43ac7dbc9e06df198938
Priyam Uttam <priyamuttam.dev@gmail.com>
2025-01-11 23:51:24 +0530

    Added Auth using Clerk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-import { Button } from "@/components/ui/button"
+import React from "react"
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react"
 
 export default function Home() {
   return (
     <div>
-      <Button >Click me</Button>
+       <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
     </div>
   )
 }
